32a8285e3ddc76f72c8f39c91b2d850e9f504b6e
Netanel Mevorach KALFA <admin@nm-digitalhub.com>
2025-11-26 11:36:01 +0200

    fix: Remove static keyword from $pollingInterval in Widgets for Filament v4
    - Changed protected static to protected for $pollingInterval property
    - Fixes: Cannot redeclare non static StatsOverviewWidget::$pollingInterval as static
    - Filament v4 requires $pollingInterval to be non-static
    
    Files modified:
    - src/Filament/Resources/SumitWebhookResource/Widgets/SumitWebhookStatsOverview.php
    - src/Filament/Resources/WebhookEventResource/Widgets/WebhookStatsOverview.php

--- src/Filament/Resources/SumitWebhookResource/Widgets/SumitWebhookStatsOverview.php
+++ src/Filament/Resources/SumitWebhookResource/Widgets/SumitWebhookStatsOverview.php
@@ -11,7 +11,7 @@ use OfficeGuy\LaravelSumitGateway\Models\SumitWebhook;
 
 class SumitWebhookStatsOverview extends BaseWidget
 {
-    protected static ?string $pollingInterval = '30s';
+    protected ?string $pollingInterval = '30s';
 
     protected function getStats(): array
     {
