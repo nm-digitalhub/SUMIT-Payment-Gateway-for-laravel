0c56c09cabaab85aa6155892852e55c941262056
nm-digitalhub <admin@nm-digitalhub.com>
2025-11-26 17:48:58 +0200

    Update OfficeGuySettings.php

--- src/Filament/Pages/OfficeGuySettings.php
+++ src/Filament/Pages/OfficeGuySettings.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace OfficeGuy\LaravelSumitGateway\Filament\Pages;
 
-use Filament\Schemas\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Notifications\Notification;
-use Filament\Pages\Page;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Schemas\Schema;
+use Filament\Forms\Components\Section;       
+  
+  use Filament\Forms\Components\Select;
+  use Filament\Forms\Components\TextInput;
+  use Filament\Forms\Components\Toggle;
+  use Filament\Notifications\Notification;
+  use Filament\Pages\Page;
+  use Filament\Forms\Concerns\InteractsWithForms;
+  use Filament\Schemas\Schema;                 
+
 use OfficeGuy\LaravelSumitGateway\Services\SettingsService;
 
 class OfficeGuySettings extends Page
